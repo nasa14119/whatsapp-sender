fbb07008063b89437b6c575e8e724addd42f7012
nasa14119 <nico.amaya0205@gmail.com>
2025-08-18 20:00:02 -0600

    fix(db): small error

--- db/index.ts
+++ db/index.ts
@@ -1,4 +1,4 @@
 import { Database } from "bun:sqlite";
 import { drizzle } from "drizzle-orm/bun-sqlite";
-const sql = new Database("./contacts.db");
+const sql = new Database(process.env.DB_CONTACT_PATH);
 export const db = drizzle({ client: sql });
